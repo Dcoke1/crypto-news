dd2f02b7ae477bda9d14bb9fae91eaac3ed6bb40
Douglas Coke <easyg@G-Macbook-Pro.local>
2022-04-28 01:27:57 -0400

    hashrouter/added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import App from "./App";
